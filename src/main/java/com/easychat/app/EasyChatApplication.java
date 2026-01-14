6d8c38cf28ae422b0eee83d0d7912a5526d2d83e
TanmayZade <tanmayzade87@gmail.com>
2026-01-15 01:46:11 +0530

    added server waking alert on login page

--- src/main/java/com/easychat/app/EasyChatApplication.java
+++ src/main/java/com/easychat/app/EasyChatApplication.java
@@ -2,9 +2,12 @@ package com.easychat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 
 
 @SpringBootApplication
+@ServletComponentScan
+
 public class EasyChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EasyChatApplication.class, args);
